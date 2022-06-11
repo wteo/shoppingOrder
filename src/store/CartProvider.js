59351e3786f984dc51b547391aa61a67785a7d40
wteo <wendyteo.wy@gmail.com>
2022-06-11 16:55:25 +0800

    Added removing item feature from kart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -12,7 +12,7 @@ const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
         
-        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id) ; 
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id); 
         // findIndex(), a built-in function in JS, which finds the index of an item in an array.
         // This function has a single parameter which you can include a function which returns a boolean value.
 
@@ -35,6 +35,25 @@ const cartReducer = (state, action) => {
             totalAmount: updatedTotalAmount
         };
     }
+    if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+
     return defaultCartState;
 };
 
