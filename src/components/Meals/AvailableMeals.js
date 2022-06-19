e35fe597e4bd64c15f32a0d8c4aa62e6b4831909
wteo <wendyteo.wy@gmail.com>
2022-06-19 14:02:05 +0800

    Added loading & error messages

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,12 +7,18 @@ import Card from '../UI/Card';
 const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async() => {
       const response = await fetch('https://shoppingorder-74d5c-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json');
       const responseData = await response.json();
       const loadedMeals = [];
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
 
       for (const key in responseData) {
         loadedMeals.push({
@@ -25,9 +31,30 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
       };
-      fetchMeals();
-      }, []);
+
+      fetchMeals().catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
+    }, []);
+
+    if (isLoading) {
+      return (
+        <section className={classes.MealsLoading}>
+          <p>Loading...</p>
+        </section>
+        );
+    }
+
+    if (httpError) {
+      return (
+        <section className={classes.MealsError}>
+          <p>Failed to fetch.</p>
+        </section>
+      );
+    }
 
     const mealsList = meals.map(meal => (
         <MealItem 
